099b0695c578cff8a505491597bd4af930273671
wolfram0407 <77099657+wolfram0407@users.noreply.github.com>
2024-06-16 22:49:05 +0900

    Feat - 락카 수정 함수 추가

--- src/router/locker.js
+++ src/router/locker.js
@@ -3,7 +3,7 @@ import { body, param } from "express-validator";
 import { db } from "../db/database.js";
 import { isAuth } from "../middleware/auth.js";
 import { validate } from "../middleware/validate.js";
-
+import dayjs from "dayjs";
 const router = express.Router();
 
 // router.post(
@@ -83,7 +83,7 @@ router.post(
 
     await db.execute(
       "INSERT INTO lockers (user_id,customer_id, lockerType_id, locker_number, charge_id, start_day, end_day,paid) VALUES (?,?,?,?,?,?,?,?)",
-      [req.authorizedUser, checkUser.idx, lockerTypeIdx, lockerNumber, periodTypeIdx, convertDate(startDate), convertDate(endDate), paid]
+      [req.authorizedUser, checkUser.idx, lockerTypeIdx, lockerNumber, periodTypeIdx, startDate, endDate, paid]
     );
 
     return res.status(200).json({
@@ -93,9 +93,29 @@ router.post(
 );
 
 // 나중에 날짜로 출력 할지 어떤 방식으로 출력할지 결정되면 진행
-router.get("/info/", isAuth, async (req, res, next) => {
+router.get("/", isAuth, async (req, res, next) => {
   try {
     const list = await db.query(`SELECT * FROM lockers WHERE user_id = ? AND deleted_at IS NULL `, [req.authorizedUser]).then((r) => r[0]);
+
+    const today = dayjs().format("YYYY-MM-DD");
+    // /*
+    // dayjs(today).diff(endDate, "day") >= 0
+    // 오늘 날짜로 부터 끝나는 날까지 계산
+    // 양수면 만료된 상태
+    // 0 이면 만료일
+    // 음수면 기간 남은 상태
+
+    // 더 나중에 날짜 diff 적은 날짜 하면 양수로 나옴
+    // */
+    // console.log(today + "  " + lockerInfo.start_day + "  " + lockerInfo.end_day);
+    // const todayStatus = dayjs(today).diff(lockerInfo.end_day, "day");
+    // // 오늘 기준 종료 날짜가 종료되어 전체 기간 + 지난 날짜
+    // // 음수면 아직 기간 남아서 시작일 부터 현재까지 계산
+    // const used = todayStatus >= 0 ? dayjs(today).diff(lockerInfo.start_day, "day") : dayjs(today).diff(lockerInfo.start_day, "day");
+    // console.log("사용한 날짜 : " + used);
+    // const remain = todayStatus >= 0 ? 0 : dayjs(lockerInfo.end_day).diff(today, "day");
+    // console.log("남은 날짜 : " + remain);
+    // //   dayjs(today).diff(start_date, "day") >= 0 ? dayjs(endDate).diff(dayjs(today), "day") : dayjs(endDate).diff(start_date, "day");
 
     return res.status(200).json({
       message: "success",
@@ -106,17 +126,39 @@ router.get("/info/", isAuth, async (req, res, next) => {
   }
 });
 
-// 나중에 날짜로 출력 할지 어떤 방식으로 출력할지 결정되면 진행
-router.get("/info/locker-type/:typeId", isAuth, async (req, res, next) => {
+//
+router.get("/type/:typeId", isAuth, async (req, res, next) => {
   try {
+    const { typeId } = req.params;
+    const lockerTypeIdList = await db
+      .execute(`SELECT * from lockers WHERE lockerType_id = ?  && user_id = ?  && deleted_at  IS NULL `, [typeId, req.authorizedUser])
+      .then((result) => result[0]);
+
     return res.status(200).json({
       message: "success",
-      data: {},
+      data: { lockerTypeIdList },
     });
   } catch (e) {
     console.log(e);
   }
 });
+
+router.get("/locker-idx/:lockerId", isAuth, async (req, res, next) => {
+  try {
+    const { lockerId } = req.params;
+    const lockerTypeIdList = await db
+      .execute(`SELECT * from lockers WHERE locker_id = ?  && user_id = ?  && deleted_at  IS NULL `, [lockerId, req.authorizedUser])
+      .then((result) => result[0]);
+
+    return res.status(200).json({
+      message: "success",
+      data: { lockerTypeIdList },
+    });
+  } catch (e) {
+    console.log(e);
+  }
+});
+
 const convertDate = (input) => {
   // 문자열을 파싱하여 연도, 월, 일로 나누기
   const year = input.substring(0, 4); // "24" -> "2024"
@@ -129,4 +171,67 @@ const convertDate = (input) => {
   const formattedDate = date.toISOString().slice(0, 19).replace("T", " ") + ".000000";
   return formattedDate;
 };
+
+// 락카 기간 수정
+// 날짜 수정을 자유롭게 만들려고 생각
+// 지속적인 관리가 어려우니 자유도를 높이는게 좋을 것 같다.
+router.put(
+  "/",
+  isAuth,
+  [
+    body("lockerIdx").trim().notEmpty().withMessage("라커 idx를 입력해 주세요."),
+    body("startDate").trim().notEmpty().withMessage("시작일을 입력해 주세요."),
+    body("endDate").trim().notEmpty().withMessage("종료일을 입력해 주세요."),
+    body("chargeId").trim().notEmpty().withMessage("요금을 입력해 주세요."),
+    body("paid").trim().notEmpty().withMessage("수납 여부를 입력해 주세요."),
+    validate,
+  ],
+  async (req, res, next) => {
+    const { lockerIdx, startDate, endDate, chargeId, paid } = req.body;
+
+    console.log(lockerIdx, startDate, endDate, chargeId, paid);
+    /* 변화 없으면 쿼리 실행 안하게 추가 하자*/
+
+    const lockerInfo = await db
+      .execute(`SELECT * FROM lockers WHERE locker_id=? &&deleted_at IS NULL`, [lockerIdx])
+      .then((result) => result[0][0]);
+
+    const newData = {
+      start_day: startDate === lockerInfo.start_day ? lockerInfo.start_day : startDate,
+      end_day: endDate === lockerInfo.end_date ? lockerInfo.end_date : endDate,
+      chargeId: chargeId === lockerInfo.charge_id ? lockerInfo.charge_id : chargeId,
+      paid: paid === lockerInfo.paid ? lockerInfo.paid : paid,
+    };
+
+    // UPDATE
+    await db.execute(`UPDATE lockers SET start_day = ?, end_day = ?, charge_id= ?, paid= ? WHERE locker_id = ?;`, [
+      newData.start_day,
+      newData.end_day,
+      newData.chargeId,
+      newData.paid,
+      lockerIdx,
+    ]);
+
+    return res.status(200).json({
+      message: "success",
+      data: { updated: newData },
+    });
+  }
+);
+// 락카 수납 수정
+
+// 락카 삭제
+
+// 라커 수리중 설정
+
+// 전체 회원 목록(라커 이용 정보) & 검색
+
+// 전체 회원 라커 상세
+
+// 사용 중인 라커 번호
+
+// [엑셀 다운로드용] 라커 전체 목록 & 검색 (항목별 오름차순/내림차순 정렬) - 리스트
+
+// 회원 정보로 메모 불러오기 (라커 이용자 추가 시 사용)
+
 export default router;
